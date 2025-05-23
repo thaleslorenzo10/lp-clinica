ac2674c664507ff41432146ab4314b03ca0852bf
thaleslorenzo10 <tplorenzo45@gmail.com>
2025-05-23 17:05:20 -0300

    Págian nova

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -8,23 +7,8 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['placeholder.svg'],
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     unoptimized: true,
   },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['lucide-react'],
-    scrollRestoration: true,
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  swcMinify: true,
-  compress: true,
-  poweredByHeader: false,
-};
+}
 
-export default nextConfig;
+export default nextConfig
