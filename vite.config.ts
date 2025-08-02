e5654e72d194deebaa359a8329b9d5d90074f1fd
thaleslorenzo10 <tplorenzo45@gmail.com>
2025-08-02 20:21:45 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    port: 8080,
+    host: "::",
+  },
   plugins: [
     react(),
     mode === 'development' &&
